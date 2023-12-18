31be0a342d686661f5361e9e34d9773b247d79f5
Hkeymer <sernakeimersamir@gmail.com>
2023-12-18 00:58:58 -0500

    uptade role default

--- src/modules/users/entities/user.entity.ts
+++ src/modules/users/entities/user.entity.ts
@@ -25,7 +25,7 @@ export class User {
   @Column({ nullable: false, select: false })
   password: string;
 
-  @Column({ type: 'enum', enum: Role, default: Role.ADMIN })
+  @Column({ type: 'enum', enum: Role, default: Role.USER })
   role: Role;
 
   @CreateDateColumn()
